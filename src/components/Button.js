e718c983b6f357c346125cfeab0cc0137fcf368e
garima verma <114860130+garimaver@users.noreply.github.com>
2023-10-02 10:58:10 +0530

    added

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,10 +2,11 @@ import React from "react";
 
 const Button = (props) => {
     return (
-        <button className="bg-pink-700 text-white hover:bg-pink-400 font[Poppins] py-2 px-6 rounded md:ml-8 duration-500">
+        <button className="bg-cyan-700 text-white hover:bg-cyan-400 font[Poppins] py-2 px-6 rounded md:ml-8 duration-500">
            {props.children}
         </button>
     )
 }
 
 export default Button
+ 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,29 +11,49 @@ const Navbar = () => {
     {name:"CONTACT", link:"/"},
    ];
 
-   let [open,setOpen]= useState(false);
+    let [open,setOpen]= useState(false);
+  //  state = {clicked: false};
+  //  handleClick = () => {
+  //   this.setState({ clicked:
+  //     !this.state.clicked } )
+  //  }
    
   return (
   <div className='shadow-md w-full fixed top-0 left-0'>
-  <div className='md:flex items-center justify-between bg-black py-4 md:px-10 px-7'>
+  <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
  
   
-    < span className='text-pink-800 hover:text-pink-400 font-bold text-4xl  cursor-pointer mr-1 pt-2'> 
+    < span className='text-cyan-800 hover:text-cyan-400 font-bold text-4xl  cursor-pointer mr-1 pt-2'> 
         Designer
         </span>
 
-<div  
-onClick={() => setOpen(!open)}
-className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden text-white'>
-<BiMenu name={open ? 'close' : 'menu'}/>
 
+{/* <div id='mobile ' className='text-cyan-800 justify-center'
+onClick={this.handleClick}>
+  <i id='bar'
+className= {this.state.clicked ? "fas fa-times" : "fas fa-bars"}>
+  </i>
+</div> */}
+
+
+
+
+
+
+
+
+
+ <div  
+onClick={() => setOpen(!open)}
+ className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden  '>  
+ <BiMenu className='text-cyan-400' />
 </div>
 
-        <ul className={`md:flex md:items-center bg-cyan-200 md:z-auto z-[1] left-0 w-full md:w-auto md:pb-0 pb-12 absolute md:static md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20': 'top-[-490px]'} md:opacity-100 opacity-0` }>
+         <ul className={`md:flex md:items-center bg-cyan-200 md:z-auto z-[1] left-0 w-full md:w-auto md:pb-0 pb-12 absolute md:static md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20': 'top-[-490px]'} md:opacity-100 opacity-0` }> 
       {
         Links.map((link)=> (
 <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
-    <a href={link.link} className='text-white hover:text-pink-400 font-bold'>{link.name}</a>
+    <a  href={link.link} className='text-cyan-800 hover:text-cyan-400 font-bold'>{link.name}</a>
 </li>
         ) )
       }
